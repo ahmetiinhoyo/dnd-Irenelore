d3006799e5bc489c1affc41d5edd1ba77bc2cae6
ahmetiinhoyo <ahmetiinhoyo@users.noreply.github.com>
2026-09-13 14:41:05 +0000

    deploy: 402f5e372ba7412dffece5fe21a6121ad271066f

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d4fcfb44.js',
+                path_to_root + 'searchindex-ff530be5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
